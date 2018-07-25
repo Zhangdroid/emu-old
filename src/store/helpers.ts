fb7e3096784a7d24094afcae6495d2b2ca0cd5c8
Zhuo Zhang <contrary.zhuo@gmail.com>
2018-07-25 22:42:25 +0800

    init action helpers

---
+++ src/store/helpers.ts
@@ -0,0 +1,18 @@
+import { Action, ActionCreatorsMapObject } from "redux";
+
+export type ActionUnion<A extends ActionCreatorsMapObject> = ReturnType<
+  A[keyof A]
+  >;
+
+interface ActionWithPayload<T extends string, P> extends Action<T> {
+  payload: P;
+}
+
+export function createAction<T extends string>(type: T): Action<T>;
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
+export function createAction<T extends string, P>(type: T, payload?: P) {
+  return payload === undefined ? { type } : { type, payload };
+}
