25564b0216f9b4dee50e8790783c6477c341044f
Zhuo Zhang <contrary.zhuo@gmail.com>
2018-07-26 21:18:40 +0800

    styles: init variables

---
+++ src/styles/variables.ts
@@ -0,0 +1,18 @@
+import { rgba } from "polished";
+
+/**
+ * Color from Material Design
+ * @see https://material.io/guidelines/style/color.html
+ */
+export const color = {
+  gery: {
+    50: "#FAFAFA",
+    100: "#F5F5F5",
+    400: "#BDBDBD",
+    800: "#424242"
+  },
+  yellow: {
+    100: "#FFF9C4",
+    200: "#FFF59D"
+  }
+};
